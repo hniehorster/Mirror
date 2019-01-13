5be49b6ff8206f5401028f4d431adedc24e259c6
Hjalte Niehorster <h.niehorster@nbproducten.nl>
2019-01-13 20:55:03 +0100

    Clock, fixed preleading zero's issue

--- modules/Clock/clock.js
+++ modules/Clock/clock.js
@@ -3,12 +3,23 @@ setInterval(function() {
     var dt = new Date();
 
     var seconds = dt.getSeconds();
+    var minutes = dt.getMinutes();
+    var hours   = dt.getHours();
 
     if(seconds < 10){
         seconds = '0' + seconds;
     }
 
-    var time = dt.getHours() + ":" + dt.getMinutes() + ":" + seconds;
+    if(minutes < 10){
+        minutes = '0' + minutes;
+    }
+
+    if(hours < 10){
+        hours = '0' + hours;
+    }
+
+
+    var time = hours + ":" + minutes + ":" + seconds;
 
     $( "#Clock > div.app_content" ).text(time);
 }, 1000);
