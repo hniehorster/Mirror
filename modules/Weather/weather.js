9b09fd566cbc23f41961a1b4bea73eeaf84891d1
Hjalte Niehorster <h.niehorster@nbproducten.nl>
2019-01-13 20:54:45 +0100

    Weather, added local development and applied some styling

--- modules/Weather/weather.js
+++ modules/Weather/weather.js
@@ -9,14 +9,18 @@ var iconLocation        = "modules/Weather/icons/";
 
 /******** END SETTINGS **********/
 
-var weatherURL = "https://api.darksky.net/forecast/" + weatherApiKey + "/" + weatherLatitude + "," + weatherLongtitude +"?units=si";
+//DEBUG-LOCAL var weatherURL = "https://api.darksky.net/forecast/" + weatherApiKey + "/" + weatherLatitude + "," + weatherLongtitude +"?units=si";
+var weatherURL = "modules/Weather/weather.json";
 
-var weatherWidget =
+console.log(weatherURL);
 
+/*** START FUNCTION ****/
 $.when(
-    $.ajax({ url: weatherURL, type: 'GET', dataType: 'jsonp'})
+    //$.ajax({ url: weatherURL, type: 'GET', dataType: 'jsonp'})
+    $.getJSON(weatherURL)
 ).then(function( data, textStatus, jqXHR ){
 
+    console.log("Data loaded" + data);
     //let's do some rounding
     var temperature = Math.round(data.currently.temperature);
 
@@ -44,11 +48,25 @@ $.when(
     var tomorrowHigh = Math.round(data.daily.data[0].temperatureHigh);
 
     tomorrow1.setDate(tomorrow.getDate()+1);
+
+    var tomorrowLow1 = Math.round(data.daily.data[1].temperatureLow);
+    var tomorrowHigh1 = Math.round(data.daily.data[1].temperatureHigh);
+
     tomorrow2.setDate(tomorrow1.getDate()+1);
+
+    var tomorrowLow2 = Math.round(data.daily.data[2].temperatureLow);
+    var tomorrowHigh2 = Math.round(data.daily.data[2].temperatureHigh);
+
     tomorrow3.setDate(tomorrow2.getDate()+1);
+
+    var tomorrowLow3 = Math.round(data.daily.data[3].temperatureLow);
+    var tomorrowHigh3 = Math.round(data.daily.data[3].temperatureHigh);
+
     tomorrow4.setDate(tomorrow3.getDate()+1);
-    tomorrow5.setDate(tomorrow4.getDate()+1);
-    tomorrow6.setDate(tomorrow5.getDate()+1);
+
+    var tomorrowLow4 = Math.round(data.daily.data[4].temperatureLow);
+    var tomorrowHigh4 = Math.round(data.daily.data[4].temperatureHigh);
+
 
     $("#Weather > div.app_content").html('<div class="weatherWidget">' +
         '<div class="weatherHeader">' +
@@ -61,11 +79,30 @@ $.when(
         '<div class="weatherForecast">' +
             '<div class="forecastDay">' +
                 '<div class="forecastText">' + weekday[tomorrow.getDay()] + '</div>' +
-                '<div class="forecastTemperature">' + tomorrowLow + ' &#8451; / ' + tomorrowHigh + ' &#8451;</div>' +
+                '<div class="forecastTemperature">' + tomorrowLow + ' <sup>&#8451;</sup> / ' + tomorrowHigh + ' <sup>&#8451;</sup></div>' +
                 '<div class="forecastIcon"><img src="' + iconLocation + data.daily.data[0].icon + '.png"</div>' +
             '</div>' +
+            '<div class="forecastDay">' +
+                '<div class="forecastText">' + weekday[tomorrow1.getDay()] + '</div>' +
+                '<div class="forecastTemperature">' + tomorrowLow1 + ' <sup>&#8451;</sup> / ' + tomorrowHigh1 + ' <sup>&#8451;</sup></div>' +
+                '<div class="forecastIcon"><img src="' + iconLocation + data.daily.data[1].icon + '.png"</div>' +
+            '</div>' +
+            '<div class="forecastDay">' +
+                '<div class="forecastText">' + weekday[tomorrow2.getDay()] + '</div>' +
+                '<div class="forecastTemperature">' + tomorrowLow2 + ' <sup>&#8451;</sup> / ' + tomorrowHigh2 + ' <sup>&#8451;</sup></div>' +
+                '<div class="forecastIcon"><img src="' + iconLocation + data.daily.data[2].icon + '.png"</div>' +
+            '</div>' +
+            '<div class="forecastDay">' +
+                '<div class="forecastText">' + weekday[tomorrow3.getDay()] + '</div>' +
+                '<div class="forecastTemperature">' + tomorrowLow3 + ' <sup>&#8451;</sup> / ' + tomorrowHigh3 + ' <sup>&#8451;</sup></div>' +
+                '<div class="forecastIcon"><img src="' + iconLocation + data.daily.data[3].icon + '.png"</div>' +
+            '</div>' +
+            '<div class="forecastDay">' +
+                '<div class="forecastText">' + weekday[tomorrow4.getDay()] + '</div>' +
+                '<div class="forecastTemperature">' + tomorrowLow4 + ' <sup>&#8451;</sup> / ' + tomorrowHigh4 + ' <sup>&#8451;</sup></div>' +
+                '<div class="forecastIcon"><img src="' + iconLocation + data.daily.data[4].icon + '.png"</div>' +
+            '</div>' +
         '</div>');
-
-    console.log(data.daily);
 });
 
+/*** END START FUNCTION ***/
