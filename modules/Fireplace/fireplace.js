f3598819fc341faf10f6bf7a0bbe4401bb3c51f7
Hjalte Niehorster <h.niehorster@nbproducten.nl>
2019-09-22 20:28:18 -0400

    Fireplace || Added auto loading of the video and added trigger area to close the video, top screen.

---
+++ modules/Fireplace/fireplace.js
@@ -0,0 +1,30 @@
+//?autoplay=1&showinfo=0&controls=0
+
+var videoURL = "https://www.youtube.com/embed/ZY3J3Y_OU0w/?autoplay=1&controls=0"
+
+$("#Fireplace > div.app_content").html('' +
+    '<div id="FireplaceClose">' +
+    '</div>'+
+    '<div class="FireplaceWidget">' +
+        '<div class="FirePlace">' +
+            '<iframe src="" width="100%" height="100%" frameborder="0" id="FireplaceSrc" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>' +
+        '</div>'+
+    '</div>');
+
+//As this App is fullscreen, create touch area to close app.
+
+
+//When Fireplace Open trigger start video
+$( document ).on( "Fireplace:open", function(){
+    console.log('Fireplace open triggered');
+    $("#FireplaceSrc").attr('src', videoURL);
+});
+
+//When Fireplace Close trigger stop video
+$(document).on("Fireplace:close", function(){
+    $("#FireplaceSrc").attr('src', "");
+});
+
+$(document).on('click', '#FireplaceClose', function(){
+    closeApp('Fireplace');
+});
